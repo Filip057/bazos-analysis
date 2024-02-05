0dfe974b652efe89a3aca8b2724f6546c6eaa77f
filip <filip.prudek@seznam.cz>
2024-02-05 11:10:11 +0100

    creating models

--- app.py
+++ app.py
@@ -1,7 +1,48 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bazos_cars.db'
+db = SQLAlchemy(app)
+
+
+class CarBrand(db.Model):
+    __tablename__ = 'car_brands'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    brand = db.Column(db.String(50))
+
+class BrandModel(db.Model):
+    __tablename__ = 'brand_models'
+
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(50))
+
+    # Relationship with BrandModel
+    models = db.relationship('BrandModel', backref='brand', lazy='joined')
+    brand_id = db.Column(db.Integer, db.ForeignKey('car_brands.id'), nullable=False)
+    
+    # Relationship with ModelInstance
+    instances = db.relationship('ModelInstance', backref='model', lazy='joined')
+
+class ModelInstance(db.Model):
+    __tablename__ = 'car_details'
+
+    id = db.Column(db.Integer, primary_key=True)
+    year_manufacture = db.Column(db.Integer)
+    mileage = db.Column(db.Integer)
+    power = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+
+    model_id = db.Column(db.Integer, db.ForeignKey('brand_models.id'), nullable=False)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
